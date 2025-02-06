f612e4fc37e44dd6fb5bddf6da7ca8e30ba1aef6
yazreyesrz <ryazru1993@gmail.com>
2025-02-05 18:49:57 -0600

    "Se modifico un archivo dentro de la carpeta Activities "

--- app/src/main/java/com/ramirinter/lologin/MainActivity.kt
+++ app/src/main/java/com/ramirinter/lologin/MainActivity.kt
@@ -1,27 +1,18 @@
 package com.ramirinter.lologin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import com.ramirinter.lologin.repository.AuthRepository
-import com.ramirinter.lologin.ui.theme.LologinTheme
-import com.ramirinter.lologin.view.LoginScreen
-import com.ramirinter.lologin.viewmodel.LoginViewModel
-import com.ramirinter.lologin.viewmodel.LoginViewModelFactory
+import com.ramirinter.lologin.activities.LoginActivity
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: LoginViewModel by viewModels {
-        LoginViewModelFactory(AuthRepository())
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LologinTheme {
-                LoginScreen(viewModel, this)  // Pasar el contexto para navegar
-            }
-        }
+
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
---
+++ app/src/main/java/com/ramirinter/lologin/activities/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.ramirinter.lologin.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import com.ramirinter.lologin.repository.AuthRepository
+import com.ramirinter.lologin.ui.theme.LologinTheme
+import com.ramirinter.lologin.view.LoginScreen
+import com.ramirinter.lologin.viewmodel.LoginViewModel
+import com.ramirinter.lologin.viewmodel.LoginViewModelFactory
+
+class LoginActivity : ComponentActivity() {
+
+    private val viewModel: LoginViewModel by viewModels {
+        LoginViewModelFactory(AuthRepository())
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LologinTheme {
+                LoginScreen(viewModel, this)
+            }
+        }
+    }
+}
+
+
